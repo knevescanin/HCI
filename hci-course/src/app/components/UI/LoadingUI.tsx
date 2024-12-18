863cf6091c6fded1669db1b62904d18afc489c42
abotica <andrija.botica1613@icloud.com>
2024-12-18 16:50:19 +0100

    Remove FontAwesome dependencies and related imports from package.json and LoadingUI component

--- hci-course/src/app/components/UI/LoadingUI.tsx
+++ hci-course/src/app/components/UI/LoadingUI.tsx
@@ -1,11 +1,11 @@
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function LoadingUI() {
     return (
         <div>
-            <FontAwesomeIcon size="xs" icon={faSpinner} spin style={{color: "#ffffff",}} />
-
+            {/* <FontAwesomeIcon size="xs" icon={faSpinner} spin style={{color: "#ffffff",}} /> */}
+            Loading...
         </div>
     );
 }
