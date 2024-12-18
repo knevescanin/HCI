6248834d336893bf2797abef816ff7df464c0b79
abotica <andrija.botica1613@icloud.com>
2024-12-18 10:42:16 +0100

    Change var to let for contentful client declaration

--- hci-course/src/app/utils/contentfulClient.ts
+++ hci-course/src/app/utils/contentfulClient.ts
@@ -1,7 +1,7 @@
 import * as contentful from 'contentful'
 
 // contentful client
-	var client = contentful.createClient({
+	let client = contentful.createClient({
 		space: process.env.CONTENTFUL_SPACE_ID,
 		accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 	})
