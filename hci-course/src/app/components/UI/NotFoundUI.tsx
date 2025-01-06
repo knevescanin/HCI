8ff816e1c6ccbbb0149e8ddf92c4c0009d9c59d1
abotica <andrija.botica1613@icloud.com>
2025-01-06 16:38:03 +0100

    Update NotFoundUI component styles: change text color to purple

--- hci-course/src/app/components/UI/NotFoundUI.tsx
+++ hci-course/src/app/components/UI/NotFoundUI.tsx
@@ -3,7 +3,7 @@ import ButtonUI from "./ButtonUI";
 
 export default function NotFoundUI() {
     return (
-        <div className="text-white flex flex-col w-full min-h-full items-center justify-center">
+        <div className="text-purple-500 flex flex-col w-full min-h-full items-center justify-center">
             Woops... Page not found, sorry!
             <ButtonUI textSize="xl"><Link href="/">Take me home</Link></ButtonUI>
         </div>
