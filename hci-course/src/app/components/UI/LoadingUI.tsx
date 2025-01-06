f8a08d879ed4e1172f3f555a8a68d7f08b4ef43a
abotica <andrija.botica1613@icloud.com>
2025-01-06 16:09:59 +0100

    Update LoadingUI component styles: change text color to purple and center align the loading message

--- hci-course/src/app/components/UI/LoadingUI.tsx
+++ hci-course/src/app/components/UI/LoadingUI.tsx
@@ -3,7 +3,7 @@
 
 export default function LoadingUI() {
     return (
-        <div className="text-white text-3xl font-bold">
+        <div className="text-purple-600 text-3xl font-bold text-center align-middle">
             {/* <FontAwesomeIcon size="xs" icon={faSpinner} spin style={{color: "#ffffff",}} /> */}
             Loading...
         </div>
