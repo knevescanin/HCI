c241cc6aea5455e5350a4c19d43e674cf7787183
abotica <andrija.botica1613@icloud.com>
2025-01-16 11:42:15 +0100

    Refactor LoadingUI component: comment out FontAwesomeIcon import and spinner for simplified loading state

--- hci-course/src/app/components/UI/LoadingUI.tsx
+++ hci-course/src/app/components/UI/LoadingUI.tsx
@@ -1,10 +1,11 @@
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function LoadingUI() {
     return (
         <div className="text-purple-600 text-3xl font-bold text-center align-middle">
-            <FontAwesomeIcon size="xs" icon={faSpinner} spin style={{color: "#ffffff",}} />
+            {/* <FontAwesomeIcon size="xs" icon={faSpinner} spin style={{color: "#ffffff",}} /> */}
+            Loading...
         </div>
     );
 }
