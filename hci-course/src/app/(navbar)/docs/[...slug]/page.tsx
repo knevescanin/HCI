fd4b4aa06dfb8aea36dbcbff802fb72199d90037
abotica <andrija.botica1613@icloud.com>
2025-01-16 14:47:02 +0100

    Refactor navigation content retrieval: improve formatting and add media field to enhance documentation display

--- hci-course/src/app/(navbar)/docs/[...slug]/page.tsx
+++ hci-course/src/app/(navbar)/docs/[...slug]/page.tsx
@@ -1,28 +1,36 @@
 import { unstable_cache } from 'next/cache'
-import {client} from '../../../utils/contentfulClient'
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { client } from '../../../utils/contentfulClient'
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 // import {BLOCKS} from '@contentful/rich-text-types';
 
+const getCurrentLocation = unstable_cache(
+	async (params: { slug: string[] }) => {
+		const locations = params.slug
+		const location = locations.join('/')
+		const formattedLocation = '/' + location
 
-const getCurrentLocation = unstable_cache(async (params: { slug: string[] }) => {
-	const locations = params.slug
-	const location = locations.join('/')
-	const formattedLocation = '/' + location
-
-	return formattedLocation
-})
+		return formattedLocation
+	}
+)
 
 const getNavigationsContent = unstable_cache(async () => {
 	const entries = await client.getEntries({
-        content_type: "docsNavigationPages",
-        select: ['fields'],
-        order: ['sys.createdAt']
-    })
+		content_type: 'docsNavigationPages',
+		select: ['fields'],
+		order: ['sys.createdAt'],
+	})
 
-    const navigationsContent = entries.items.map(entry => {
-        const path = (entry.fields.path as any)?.path;
-        return {header: entry.fields.header, richTextString: documentToHtmlString(entry.fields.content as any, {preserveWhitespace: true}), path: path, media: entry.fields.media};
-    });
+	const navigationsContent = entries.items.map((entry) => {
+		const path = (entry.fields.path as any)?.path
+		return {
+			header: entry.fields.header,
+			richTextString: documentToHtmlString(entry.fields.content as any, {
+				preserveWhitespace: true,
+			}),
+			path: path,
+			media: entry.fields.media,
+		}
+	})
 
 	return navigationsContent
 })
@@ -39,16 +47,17 @@ export default async function page({
 
 	return (
 		<>
-		
 			<div
 				className="w-full h-max text-black"
 				dangerouslySetInnerHTML={{
 					__html:
-						navigationsContent.find(
-							(nav) => nav.path === currentLocation
-						)?.richTextString || '',
-				}}></div>
-			
+						navigationsContent.find((nav) => nav.path === currentLocation)
+							?.richTextString || '',
+				}}>
+
+				</div>
+
+			<img src="" alt="" />
 		</>
 	)
 }
