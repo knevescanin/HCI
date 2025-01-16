8de3814dd4dfaeb12e6542c14453df7b6c572280
abotica <andrija.botica1613@icloud.com>
2025-01-16 11:40:20 +0100

    Enhance LoadingUI component: uncomment FontAwesomeIcon import and loading spinner for improved user feedback

--- hci-course/src/app/components/UI/LoadingUI.tsx
+++ hci-course/src/app/components/UI/LoadingUI.tsx
@@ -1,11 +1,10 @@
-// import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function LoadingUI() {
     return (
         <div className="text-purple-600 text-3xl font-bold text-center align-middle">
-            {/* <FontAwesomeIcon size="xs" icon={faSpinner} spin style={{color: "#ffffff",}} /> */}
-            Loading...
+            <FontAwesomeIcon size="xs" icon={faSpinner} spin style={{color: "#ffffff",}} />
         </div>
     );
 }
