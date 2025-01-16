5f4fcc098bbf3392db65766219a73b57928d36f1
abotica <andrija.botica1613@icloud.com>
2025-01-16 11:58:33 +0100

    Refactor documentation page: remove loading state and enhance content structure for improved user experience

--- hci-course/src/app/(navbar)/docs/page.tsx
+++ hci-course/src/app/(navbar)/docs/page.tsx
@@ -1,22 +1,11 @@
-"use client"
-
-import LoadingUI from '@/app/components/UI/LoadingUI';
-import { useState, useEffect } from 'react';
 
 export default function Page() {
-	const [isLoading, setIsLoading] = useState(true);
 
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setIsLoading(false);
-		}, 30000); // 3 seconds timeout
-
-		return () => clearTimeout(timer);
-	}, []);
 
 	return (
 		<div>
-			{isLoading ? <LoadingUI /> : <div>Content Loaded</div>}
+			<h1>Introduction</h1>
+			<p>Welcome to the documentation page. Here you will find all the information you need to get started.</p>
 		</div>
 	);
 }
