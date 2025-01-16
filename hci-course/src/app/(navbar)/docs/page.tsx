3091b1486b601ef4663aeb57ab60a44fd4d2ce5a
abotica <andrija.botica1613@icloud.com>
2025-01-16 11:55:35 +0100

    Enhance loading experience: implement loading state with timeout and restore FontAwesome spinner in LoadingUI component

--- hci-course/src/app/(navbar)/docs/page.tsx
+++ hci-course/src/app/(navbar)/docs/page.tsx
@@ -1,10 +1,22 @@
+"use client"
 
+import LoadingUI from '@/app/components/UI/LoadingUI';
+import { useState, useEffect } from 'react';
 
-export default function page() {
-	
+export default function Page() {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setIsLoading(false);
+		}, 30000); // 3 seconds timeout
+
+		return () => clearTimeout(timer);
+	}, []);
+
 	return (
 		<div>
-			
+			{isLoading ? <LoadingUI /> : <div>Content Loaded</div>}
 		</div>
-	)
+	);
 }
--- hci-course/src/app/components/UI/LoadingUI.tsx
+++ hci-course/src/app/components/UI/LoadingUI.tsx
@@ -1,11 +1,11 @@
-// import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function LoadingUI() {
     return (
-        <div className="text-purple-600 text-3xl font-bold text-center align-middle">
-            {/* <FontAwesomeIcon size="xs" icon={faSpinner} spin style={{color: "#ffffff",}} /> */}
-            Loading...
+        <div className="flex justify-center items-center">
+            <FontAwesomeIcon size="2xl" spin icon={faSpinner} color="#4E00AF" />
+            {/* Loading... */}
         </div>
     );
 }
