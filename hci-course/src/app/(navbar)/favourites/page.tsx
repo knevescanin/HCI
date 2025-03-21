5edb6e5176f962c3287033369e56c18ef68fb202
abotica <andrija.botica1613@icloud.com>
2025-03-21 20:18:05 +0100

    Add favourites page: create a new page to display user's favorite products using ProductCard component

---
+++ hci-course/src/app/(navbar)/favourites/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+import ProductCard from "@/app/components/ProductCard";
+
+
+function page() {
+    return (
+        <div>
+
+            <h1>User's favourites</h1>
+            
+            <div className="grid grid-cols-8 w-full h-max text-white relative bg-white">
+                <ProductCard
+                    name="Product 1"
+                    imageUrl="/images/product1.jpg"
+                    store="Store A"
+                    price={29.99}
+                />
+                <ProductCard
+                    name="Product 2"
+                    imageUrl="/images/product2.jpg"
+                    store="Store B"
+                    price={49.99}
+                />
+                <ProductCard
+                    name="Product 3"
+                    imageUrl="/images/product3.jpg"
+                    store="Store C"
+                    price={19.99}
+                />
+                <ProductCard
+                    name="Product 4"
+                    imageUrl="/images/product4.jpg"
+                    store="Store D"
+                    price={39.99}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default page;
--- hci-course/src/app/components/ProductCard.tsx
+++ hci-course/src/app/components/ProductCard.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import Link from "next/link"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons"
 import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons"
