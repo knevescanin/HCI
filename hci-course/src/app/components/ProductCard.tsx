c3bc64d1039fa43c949a026420e2e158bc7ffcad
abotica <andrija.botica1613@icloud.com>
2025-03-21 20:02:42 +0100

    Update ProductCard component: add favorite icon toggle functionality and conditionally render based on user login status

--- hci-course/src/app/components/ProductCard.tsx
+++ hci-course/src/app/components/ProductCard.tsx
@@ -23,11 +23,13 @@ export default function ProductCard({
 	function handleOnClick() {
 		setIsFavourite(!isFavourite)
 		//send to backend
+		
 	}
     
 	return (
         <div className="bg-[#FFFFFF] rounded-xl p-4 m-1 flex flex-col items-center relative w-full h-max hover:shadow-lg transition duration-300 ease-in-out">
             <div className="w-full h-2/3 mb-4 relative">
+			{/* show favourite icon if user is logged in, else don't */}
 			<FontAwesomeIcon onClick={handleOnClick} icon={isFavourite ? faHeartSolid : faHeartRegular} className="absolute top-2 right-2 text-textPrimary hover:scale-110 hover:cursor-pointer" />
             <Image
 				unoptimized
