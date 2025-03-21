ea86400c4147e7111b19c0846fa2a8e09a808a66
abotica <andrija.botica1613@icloud.com>
2025-03-21 20:01:30 +0100

    Add favorite functionality to ProductCard component: implement state management for favorites and update UI with FontAwesome icons

--- hci-course/src/app/components/ProductCard.tsx
+++ hci-course/src/app/components/ProductCard.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image"
 import Link from "next/link"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons"
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
 
 export default function ProductCard({
 	name,
@@ -13,10 +17,18 @@ export default function ProductCard({
 	price: number
 }) 
 {
+
+	const [isFavourite, setIsFavourite] = useState(false)
+
+	function handleOnClick() {
+		setIsFavourite(!isFavourite)
+		//send to backend
+	}
     
 	return (
-        <Link href={""} className="bg-[#FFFFFF] rounded-xl p-4 m-1 flex flex-col items-center relative w-full h-max hover:shadow-lg transition duration-300 ease-in-out">
-            <div className="w-full h-2/3 mb-4">
+        <div className="bg-[#FFFFFF] rounded-xl p-4 m-1 flex flex-col items-center relative w-full h-max hover:shadow-lg transition duration-300 ease-in-out">
+            <div className="w-full h-2/3 mb-4 relative">
+			<FontAwesomeIcon onClick={handleOnClick} icon={isFavourite ? faHeartSolid : faHeartRegular} className="absolute top-2 right-2 text-textPrimary hover:scale-110 hover:cursor-pointer" />
             <Image
 				unoptimized
                 src={imageUrl}
@@ -29,6 +41,6 @@ export default function ProductCard({
 			<h2 className="text-textPrimary font-latoBlack text-lg line-clamp-1">{name}</h2>
 			<p className="text-gray-500">{store}</p>
 			<p className="text-textPrimary font-latoBlack">{price.toFixed(2)} €</p>
-		</Link>
+		</div>
 	)
 }
