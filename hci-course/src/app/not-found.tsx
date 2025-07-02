5d9ded6f07007f74b291bfe00f8ed63bf87d3793
knevescanin <karlo.nevescanin21@gmail.com>
2025-07-02 13:48:55 +0200

    Add NotFound component to handle missing pages and improve user navigation

--- hci-course/src/app/not-found.tsx
+++ hci-course/src/app/not-found.tsx
@@ -1,5 +1,5 @@
 import NotFoundUI from "./components/UI/NotFoundUI";
-
+// This one is default not-found page (not for those Links inside footer).
 export default function NotFound() {
     return (
         <NotFoundUI />
---
+++ hci-course/src/app/not-found/page.tsx
@@ -0,0 +1,7 @@
+import NotFoundUI from "../components/UI/NotFoundUI";
+// This one is for those Links inside footer, that currently lead to nothing. 
+export default function NotFound() {
+    return (
+        <NotFoundUI />
+    );
+}
